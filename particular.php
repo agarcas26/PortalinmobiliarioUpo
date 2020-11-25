31cfe9214cd94163573757c7c3b880d5861ebf3d
Andrea Garcia <agarcas@alu.upo.es>
2020-11-25 12:39:45 +0100

    clase particular

--- particular.php
+++ particular.php
@@ -1,8 +1,50 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Cuenta Usuario Particular </title>
+    </head>
+    <body>
+        <header>
+            <div id="encabezadoParticular">
+                <h1>Hola <?php $_POST["nombre_usuario"]?> !Bienvenido a tu area de gestión</h1>
+                <br><br>
+                 <a href="anunciate.php">Publica tu anuncio gratis</a>
+            </div>
+        </header>
+       
+       
+        <nav>
+            <a>General</a>
+            <a>Anuncios</a>
+            <a>Mis Alertas</a>
+            <a>Mis Mensajes</a>
+            <a>Mi perfil</a>
+        </nav>
+        <?php
+        if (!isset($_POST['enviar'])) {
+            ?>
+            <form action="registro.php" method="POST">
+                Nombre: <input type="text" id="nombre_usuario" name="nombre_usuario" value="" />
+                Usuario: <input type="text" id="usuario" name="usuario" value="" />
+                Contraseña: <input type="password" id="contrasena" name="contrasena" value="" />
+                Confirmar contraseña: <input type="password" id="conf_contrasena" name="conf_contrasena" value="" />
 
+                <input type="submit" id="enviar" name="enviar" value="Confirmar registro" />
+            </form>
+            <?php
+        } else {
+            $nombre_usuario = $_POST['nombre_usuario'];
+            $usuario = $_POST['usuario'];
+            //Comprobar que no haya usuarios repetidos en la bbdd
+            
+            $contrasena = $_POST['contrasena'];
+            $conf_contrasena = $_POST['conf_contrasena'];
+            
+            if($contrasena === $conf_contrasena){
+                //Insertamos los datos en la bbdd
+            }
+        }
+        ?>
+    </body>
+</html>
