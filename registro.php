d11cd68f27c347637e8b745a690fa375b2f29d0a
raqueltezme <rabenzmer@gmail.com>
2020-11-25 12:29:47 +0100

    Update registro.php

--- registro.php
+++ registro.php
@@ -11,10 +11,21 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+
+        function conexion1__bbdd() {
+            $host = 'localhost';
+            $dbname = 'inmobiliaria';
+            $port = 3306;
+            $username = 'root';
+            $passwd = '';
+            return mysqli_connect($host, $username, $passwd, $dbname);
+        }
+
         if (!isset($_POST['enviar'])) {
             ?>
             <form action="registro.php" method="POST">
                 Nombre: <input type="text" id="nombre_usuario" name="nombre_usuario" value="" />
+                Email: <input type="text" id="email" name="email" value="" />
                 Usuario: <input type="text" id="usuario" name="usuario" value="" />
                 Contraseña: <input type="password" id="contrasena" name="contrasena" value="" />
                 Confirmar contraseña: <input type="password" id="conf_contrasena" name="conf_contrasena" value="" />
@@ -24,13 +35,14 @@ and open the template in the editor.
             <?php
         } else {
             $nombre_usuario = $_POST['nombre_usuario'];
+            $email = $_POST['email'];
             $usuario = $_POST['usuario'];
-            //Comprobar que no haya usuarios repetidos en la bbdd
-            
+            //Comprobar que no haya usuarios repetidos ni correos repetidos en la bbdd
+
             $contrasena = $_POST['contrasena'];
             $conf_contrasena = $_POST['conf_contrasena'];
-            
-            if($contrasena === $conf_contrasena){
+
+            if ($contrasena === $conf_contrasena) {
                 //Insertamos los datos en la bbdd
             }
         }
