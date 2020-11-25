8ae16bd3e4138ecaf74a55bf8538fb4e7121bde7
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2020-11-25 12:43:03 +0100

    Update login.php

--- login.php
+++ login.php
@@ -43,10 +43,10 @@ if (isset($_POST['registro'])) {
         if (mysqli_num_rows($result) != 0) {
             echo '<h1>Acceso permitido</h1>';
 
-            if (getTipoUsusario($entradas[user]) = "Particualar")
+            if (getTipoUsusario($entradas[user]) = "Profesional")
                 header('Location: profesionales.php');
 
-            if (getTipoUsusario($entradas[user]) = "Particualar")
+            if (getTipoUsusario($entradas[user]) = "Particular")
                 header('Location: particular.php');
         } else
             echo '<h1>Acceso denegado</h1>';
