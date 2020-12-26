d8cf9ec3c3c5c422bd1a4199d485dd13dd198f2e
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2020-12-26 20:40:03 +0100

    a

--- perfil.php
+++ perfil.php
@@ -47,31 +47,31 @@
         <main>
             <h3>Imagen de perfil:</h3>
             <?php
-            if(!isset($_SESSION('searchuser'))){                //opcion exclusiva para admins
+            if(!isset($_SESSION['searchuser'])){                //opcion exclusiva para admins
                 echo "<figure>";
-                echo getusuario($_SESSION('searchuser'))[2] ;
+                echo getusuario($_SESSION['searchuser'])[2] ;
                 echo "</figure>";
             }else{                                              //opcion para usuarios
                 echo "<figure>";
-                echo getusuario($_SESSION('user'))[2]; 
+                echo getusuario($_SESSION['user'])[2]; 
                 echo "</figure>";
             }
             ?>
             
             <h3>Nombre usuario</h3>
             <?php
-            if(!isset($_SESSION('searchuser'))){                //opcion exclusiva para admins
-                echo getusuario($_SESSION('searchuser'))[0];
+            if(!isset($_SESSION['searchuser'])){                //opcion exclusiva para admins
+                echo getusuario($_SESSION['searchuser'])[0];
             }else{                                              //opcion para usuarios
-                echo getusuario($_SESSION('user'))[0]; 
+                echo getusuario($_SESSION['user'])[0]; 
             }
             ?>
             <h3>Correo</h3>
             <?php
-            if(!isset($_SESSION('searchuser'))){                //opcion exclusiva para admins
-                echo getusuario($_SESSION('searchuser'))[1];
+            if(!isset($_SESSION['searchuser'])){                //opcion exclusiva para admins
+                echo getusuario($_SESSION['searchuser'])[1];
             }else{                                              //opcion para usuarios
-                echo getusuario($_SESSION('user'))[1]; 
+                echo getusuario($_SESSION['user'])[1]; 
             }
             ?>
             
@@ -81,7 +81,7 @@
 
     <?php
     if (isset($_POST['logout'])) {
-        $_SESSION['user'] = NULL;
+        unset($_SESSION['user']);
         header('Location: login.php');
     }
     ?>
