426065c4f2af85ab83084880e318af0f3899354d
Andrea Garcia <agarcas@alu.upo.es>
2020-12-28 16:56:08 +0100

    dao anuncios

---
+++ Dao/daoanuncios.php
@@ -0,0 +1,30 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once '../Persistencia/Conexion.php';
+require_once '../Modelos/anuncios.php';
+require_once '../Modelos/usuario.php';
+
+class daoanuncios{
+    public $conObj;
+    public $conn;
+    private $anuncio;
+    
+    function __construct(){
+        $this->conObj = new conn(); 
+        $this->conn = $this->conObj->establecer_conexion();
+    }
+    
+    //operaciones crud 
+    
+    public function insertar($objAnuncio){
+        //paso del objeto anuncio a las variables individuales
+        
+        
+    }
+}
