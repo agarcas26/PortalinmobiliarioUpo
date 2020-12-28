c6a2c4d627e4634624e5ee6ba51fde1e7266096c
raqueltezme <rabenzmer@gmail.com>
2020-12-28 17:48:54 +0100

    a

---
+++ Modelos/registroModel.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <?php
+        include_once '../Controladores/registroController.php';
+        include_once '../DAO/registroController.php';
+        ?>
+    </head>
+    <body>
+        <?php
+        
+        function registroModel($nombre_usuario, $email, $usuario, $pass){
+            
+        }
+        
+        ?>
+    </body>
+</html>
