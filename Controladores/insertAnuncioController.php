fbd4ae3979e880bd36d7a7c12f8b6840187aad05
Andrea Garcia <agarcas@alu.upo.es>
2020-12-30 11:22:34 +0100

    dao anuncios

---
+++ Controladores/insertAnuncioController.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once '../Modelos/UsuarioModel.php';
+include_once '../Modelos/modelo_anuncios.php';
+include_once '../Dao/daoanuncios.php';
+
+session_start();
+
+$anuncio1 = new anuncios();
+
+$anuncio1->setDireccion($_POST["direccion"]);
+$anuncio1->setIdAnuncio($_POST["idAnuncio"]);
+$anuncio1->setPrecio($_POST["precio"]);
+$anuncio1->setUsuario_pk($_POST["usuario_pk"]);
+
+$daoAnuncio = new daoanuncios();
+
+$daoAnuncio->insertar($anuncio1);
+
+header('Location: ../Vistas/perfil.php ');
