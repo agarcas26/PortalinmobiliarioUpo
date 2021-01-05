17e2e8f06a596694f35d9b75044da0b1cb944e16
Andrea Garcia <agarcas@alu.upo.es>
2021-01-05 10:42:34 +0100

    dao anuncios

---
+++ Controladores/eliminaAnuncioController.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include_once '../Modelos/modelo_inmuebles.php';
+include_once '../Dao/daoinmueble.php';
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+
