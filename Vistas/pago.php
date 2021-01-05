0f4c8c11facc22e8c8d294c8eecb65164a2c02a9
raqueltezme <rabenzmer@gmail.com>
2021-01-05 12:08:56 +0100

    Update pago.php

--- Vistas/pago.php
+++ Vistas/pago.php
@@ -1,8 +1,45 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!doctype html>
+<html>
+    <head>
+        <title>Pago</title>
+        <script type="text/javascript">
+            function datosTarjeta() {
+                var tabla_datos = document.getElementById("datos_visa");
+                tabla_datos.innerHTML("<tr>"
+                        + "<td><label>Nombre titular</label><input type='text' name='titular'/></td>"
+                        + "</tr><tr>"
+                        + "<td><label>Número tarjeta</label><input type='number' name='num_tarjeta'/></td>"
+                        + "</tr>"
+                        + "<tr>"
+                        + "<td><label>Caducidad</label><input type='text' name='caducidad' /></td>"
+                        + "<td><label>CVV</label><input type='password' name='cvv' /></td>"
+                        + "</tr>");
+            }
+        </script>
+    </head>
+    <body>
+        <header>
+            <img id="logo" src="" />
+            <a href="">Inmobiliaria UPO</a>
+        </header>
+        <main>
+            <form action="psgoController.php" method="POST">
+                <input type="submit" name="paypal" value="PayPal" />
+                <input type="submit" name="visa" onclick="datosTarjeta()" value="Visa" />
+                <table id="datos_visa">
+                </table>
+                <table>
+                    <tr>
+                        <td><strong>Datos del inmueble</strong></td>
+                        <td><strong>Datos del usuario</strong></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+                <input type="submit" name="guardar" value="Confirmar pago" />
+            </form>
+        </main>
+    </body>
+</html>
