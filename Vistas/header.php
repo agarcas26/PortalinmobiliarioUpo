7fe8ee9373990555f3b2c9a254f41401af24a7e9
raqueltezme <rabenzmer@gmail.com>
2021-01-20 13:59:09 +0100

    Update header.php

--- Vistas/header.php
+++ Vistas/header.php
@@ -7,21 +7,21 @@
  */
 
 function sesion_iniciada() {
-    echo "<img src='src' alt='Logo'/>" 
-    + "<a href='index.php'>Inmobiliaria UPO</a>" 
-    + "<nav>" 
-    + "<a href='./Vistas/perfil.php'>Mi perfil</a>" 
-    + "<a href='./Vistas/mis_anuncios.php'>Mi anuncios</a>" 
-    + "</nav>";
+    echo "<img src='src' alt='Logo'/>"
+    . "<a href='index.php'>Inmobiliaria UPO</a>"
+    . "<nav class='navbar navbar-light bg-light static-top'>"
+    . "<a href='./Vistas/perfil.php'>Mi perfil</a>"
+    . "<a href='./Vistas/mis_anuncios.php'>Mi anuncios</a>"
+    . "</nav>";
 }
 
 function no_sesion_iniciada() {
-    echo "<img src='src' alt='Logo'/>" 
-    + "<a href='index.php'>Inmobiliaria UPO</a>" 
-    + "<nav>" 
-    + "<a href='login.php'>¿Ya tienes cuenta?Inicia sesión</a>"
-    + "<a href='registro.php'>¡Regístrate gratis!</a>"
-    + "</nav>";
+    echo "<img src='src' alt='Logo'/>"
+    . "<a href='../Vistas/index.php'>Inmobiliaria UPO</a>"
+    . "<nav class='navbar navbar-light bg-light static-top'>"
+    . "<a href='login.php'>¿Ya tienes cuenta?Inicia sesión</a>"
+    . "<a href='registro.php'>¡Regístrate gratis!</a>"
+    . "</nav>";
 }
 
 function cabecera_admin() {
