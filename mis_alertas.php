45b8a8b5a4ad6e9efd7324eeb8dba493b1180823
raqueltezme <rabenzmer@gmail.com>
2021-01-20 12:51:53 +0100

    .

--- mis_alertas.php
+++ mis_alertas.php
@@ -2,15 +2,26 @@
 <html>
     <head>
         <title>Mis alertas</title>
-    </head>
-    <body>
-        <header>
-            
-        </header>
-        <main>
-            <aside>
-                
-            </aside>
-        </main>
-    </body>
+        <?php
+        include_once '../PortalinmobiliarioUpo/header.php';
+        include_once '../PortalinmobiliarioUpo/aside.php';
+        if (isset($_SESSION['usuario_particular']) || isset($_SESSION['usuario_profesional']) || isset($_SESSION['admin'])) {
+            ?>
+        </head>
+        <body><header>
+                <?php sesion_iniciada(); ?>
+            </header>
+            <main>
+                <aside>
+                    <?php aside_sesion_iniciada(); ?>
+                </aside>
+        </body>
+        <footer>
+            <?php include_once '../PortalinmobiliarioUpo/Vistas/footer.html'; ?>
+        </footer>
+        <?php
+    } else {
+        header("Location: login.php");
+    }
+    ?>
 </html>
--- mis_favoritos.php
+++ mis_favoritos.php
@@ -1,14 +1,26 @@
 <!doctype html>
 <html>
     <head>
-        <title>Mis favoritos</title>
-    </head>
-    <body>
-        <header>
-            
-        </header>
-        <main>
-            
-        </main>
-    </body>
+        <title>Mis favoritos</title> <?php
+        include_once '../PortalinmobiliarioUpo/header.php';
+        include_once '../PortalinmobiliarioUpo/aside.php';
+        if (isset($_SESSION['usuario_particular']) || isset($_SESSION['usuario_profesional']) || isset($_SESSION['admin'])) {
+            ?>
+        </head>
+        <body><header>
+                <?php sesion_iniciada(); ?>
+            </header>
+            <main>
+                <aside>
+                    <?php aside_sesion_iniciada(); ?>
+                </aside>
+        </body>
+        <footer>
+            <?php include_once '../PortalinmobiliarioUpo/Vistas/footer.html'; ?>
+        </footer>
+        <?php
+    } else {
+        header("Location: login.php");
+    }
+    ?>
 </html>
