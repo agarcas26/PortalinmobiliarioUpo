1e7660711c9822b4ffbcbc22644c1406232e5dce
raqueltezme <rabenzmer@gmail.com>
2021-01-20 13:23:01 +0100

    Update daoPago.php

--- daoPago.php
+++ daoPago.php
@@ -1,1 +1,18 @@
 <?php
+
+function listar_pagos(){
+    
+    $sentence = "SELECT * FROM `pagos`";
+    $result = mysqli_query($conexion, $sentence);
+    
+    return $result;
+}
+
+function listar_pagos_usuario($nombre_usuario){
+    
+}
+
+function crear_pago(){
+    
+    
+}
