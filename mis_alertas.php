0def71ee77dee3185c92512727be70a5fe80fbb9
raqueltezme <rabenzmer@gmail.com>
2021-01-22 17:53:51 +0100

    Update mis_alertas.php

--- mis_alertas.php
+++ mis_alertas.php
@@ -3,8 +3,8 @@
     <head>
         <title>Mis alertas</title>
         <?php
-        include_once '../PortalinmobiliarioUpo/header.php';
-        include_once '../PortalinmobiliarioUpo/aside.php';
+        include_once '../Vistas/header.php';
+        include_once '../Vistas/aside.php';
         if (isset($_SESSION['usuario_particular']) || isset($_SESSION['usuario_profesional']) || isset($_SESSION['admin'])) {
             ?>
         </head>
@@ -17,9 +17,9 @@
                 </aside>
             </main>
         </body>
-        <footer>
-            <?php include_once '../PortalinmobiliarioUpo/Vistas/footer.html'; ?>
-        </footer>
+          <?php
+    include_once '../Vistas/footer.html';
+    ?>
         <?php
     } else {
         header("Location: login.php");
