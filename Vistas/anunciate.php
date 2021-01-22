835cd238e00d0e9f48a81a19252637df6d097988
raqueltezme <rabenzmer@gmail.com>
2021-01-22 21:04:07 +0100

    .

--- Vistas/anunciate.php
+++ Vistas/anunciate.php
@@ -24,46 +24,80 @@
             <?php
             if (!isset($_POST['guardar'])) {
                 ?>
-                <form>
-                    <button>Añade tus fotos</button>
-                    <label>Puedes subir varias a la vez. Recuerda subir fotos de todas las estancias.</label>
-                    <select id="tipo_inmueble" type="picker" label="Tipo de inmueble" display="" aria-invalid="false" class="">
-                        <option value="casa">Casa</option>
-                        <option value="atico">Ático</option>
-                        <option value="duplex">Dúplex</option>
-                        <option value="piso">Piso</option>
-                    </select>
-                    <ul>
-                        <li>
-                            <label class="" tabindex="0">
+            <h3>Alta de un nuevo inmueble</h3>
+                <form class="form-control-plaintext">
+                    <table class="table">
+                        <tr>
+                            <td>
+                                <input type="file" value="Añade tus fotos"/></br>
+                                <label>Puedes subir varias imágenes a la vez. Recuerda subir fotos de todas las estancias.</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <select multiple id="tipo_inmueble" type="picker" label="Tipo de inmueble" display="" aria-invalid="false" class="dropdown-menu">
+                                    <option value="casa">Casa</option>
+                                    <option value="atico">Ático</option>
+                                    <option value="duplex">Dúplex</option>
+                                    <option value="piso">Piso</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
                                 <input tabindex="-1" type="radio" name="oferta" value="venta" checked="">Vendo</label>
-                        </li>
-                        <li>
-                            <label class="" tabindex="0">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
                                 <input tabindex="-1" type="radio" name="oferta" value="alquiler">Alquilo</label>
-                        </li>
-                        <li>
-                            <label class="" tabindex="0">
-                                <input tabindex="-1" type="radio" name="oferta" value="vacacional">Vacacional</label>
-                        </li>
-                    </ul>
-                    <input placeholder="Precio de venta" id="precio" type="tel" label="Precio de venta" display="money" value="">
-                    <input placeholder="Precio de venta" id="superficie" type="tel" label="Superficie" display="surface" value="">
-                    <input class="" tabindex="-1" placeholder="Calle, número y población" value="">
-                    <label>Número de habitaciones</label>
-                    <span class="">
-                        <button>-</button>
-                        <input tabindex="0" readonly="" class="" value="2">
-                        <button>+</button>
-                    </span>
-                    <label>Número de baños</label>
-                    <span class="">
-                        <button>-</button>
-                        <input tabindex="0" readonly="" class="" value="1">
-                        <button>+</button>
-                    </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                    <input tabindex="-1" type="radio" name="oferta" value="vacacional">Vacacional</label>
+                            </td>
+                        </tr>
 
-                    <input type="submit" name="guardar" value="Guardar">
+                        <tr>
+                            <td>
+                                <input placeholder="Precio de venta" id="precio" type="tel" label="Precio de venta" display="money" value=""></br>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input placeholder="Precio de venta" id="superficie" type="tel" label="Superficie" display="surface" value=""></br>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input class="" tabindex="-1" placeholder="Calle, número y población" value=""></br>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Número de habitaciones</label>
+                                <span class="">
+                                    <button>-</button>
+                                    <input tabindex="0" readonly="" class="" value="2">
+                                    <button>+</button>
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Número de baños</label>
+                                <span class="">
+                                    <button>-</button>
+                                    <input tabindex="0" readonly="" class="" value="1">
+                                    <button>+</button>
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                        <input type="submit" name="guardar" class="btn-success" value="Guardar">
+                        </tr>
+                    </table>
                 </form>
                 <?php
             }
--- Vistas/header.php
+++ Vistas/header.php
@@ -7,7 +7,7 @@
  */
 
 function sesion_iniciada() {
-    echo "<a href='index.php'><img src='../img/logo.png' alt='Logo'/></a>"
+    echo "<a href='index.php'><img class='icon' src='../img/logo.png' alt='Logo'/></a>"
     . "<nav class='navbar navbar-light bg-light static-top'>"
     . "<a href='../Vistas/perfil.php'>Mi perfil</a>"
     . "<a href='../Vistas/mis_anuncios.php'>Mi anuncios</a>"
@@ -15,7 +15,7 @@ function sesion_iniciada() {
 }
 
 function no_sesion_iniciada() {
-    echo "<a href='../Vistas/index.php'><img src='../img/logo.png' alt='Logo'/></a>"
+    echo "<a href='../Vistas/index.php'><img class='icon' src='../img/logo.png' alt='Logo'/></a>"
     . "<nav class='navbar navbar-light bg-light static-top'>"
     . "<a href='../Vistas/login.php'>¿Ya tienes cuenta?Inicia sesión</a>"
     . "<a href='../Vistas/registro.php'>¡Regístrate gratis!</a>"
