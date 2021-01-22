fe8278f2b8f5ed86b5641acafc5768f2c95e33c5
raqueltezme <rabenzmer@gmail.com>
2021-01-22 20:10:30 +0100

    Update daoAdministradores.php

--- DAO/daoAdministradores.php
+++ DAO/daoAdministradores.php
@@ -2,14 +2,18 @@
 
 include_once '../Persistencia/Conexion.php';
 
-function listar_administradores() {
-    $sentence = "SELECT * FROM `administradores`";
-    $result = mysqli_query($conexion, $sentence);
-}
+class daoAdministradores {
 
-function get_administrador($nombre_usuario_admin, $contrasenya_admin) {
-    $sentence = "SELECT * FROM `administradores` WHERE `administradores`.`nombre_usuario_admin` = '" . $nombre_usuario_admin . "' and `administradores`.`contrasenya_admin` = '" . $contrasenya_admin . "';";
-    $result = mysqli_query($conexion, $sentence);
+    function listar_administradores() {
+        $sentence = "SELECT * FROM `administradores`";
+        $result = mysqli_query($conexion, $sentence);
+    }
 
-    return mysqli_fetch_row($result);
+    function get_administrador($nombre_usuario_admin, $contrasenya_admin) {
+        $sentence = "SELECT * FROM `administradores` WHERE `administradores`.`nombre_usuario_admin` = '" . $nombre_usuario_admin . "' and `administradores`.`contrasenya_admin` = '" . $contrasenya_admin . "';";
+        $result = mysqli_query($conexion, $sentence);
+
+        return mysqli_fetch_row($result);
+    }
+
 }
