9fd09191672163d1223d69b1e9783d06da281a8f
raqueltezme <rabenzmer@gmail.com>
2021-01-23 14:03:47 +0100

    .

--- Vistas/header.php
+++ Vistas/header.php
@@ -7,7 +7,7 @@
  */
 
 function sesion_iniciada() {
-    echo"<nav class='navbar navbar-light bg-light static-top'>"
+    echo"<nav  style='width: 100%;position:absolute; top:0; left:0;' class='navbar navbar-light bg-light static-top'>"
     . "<a href='../Vistas/perfil.php'>Mi perfil</a>"
     . "<a href='../Vistas/mis_anuncios.php'>Mi anuncios</a>"
     . "</nav>"
@@ -16,7 +16,7 @@ function sesion_iniciada() {
 }
 
 function no_sesion_iniciada() {
-    echo "<nav class='navbar navbar-light bg-light static-top'>"
+    echo "<nav  style='width: 100%;position:absolute; top:0; left:0;' class='navbar navbar-light bg-light static-top'>"
     . "<a href='../Vistas/login.php'>¿Ya tienes cuenta?Inicia sesión</a>"
     . "<a href='../Vistas/registro.php'>¡Regístrate gratis!</a>"
     . "</nav>"
