3eff5bcd31e14542a0f71bbe48703b764f2cbaf0
raqueltezme <rabenzmer@gmail.com>
2021-01-23 12:00:38 +0100

    Update UsuarioController.php

--- Controladores/UsuarioController.php
+++ Controladores/UsuarioController.php
@@ -4,7 +4,8 @@ include_once '../DAO/daoUsuarios.php';
 include_once '../Modelos/UsuarioModel.php';
 
 function getUsuarioByUsuario($nombre_usuario) {
-    $usuario_datos = get_usuario_by_nombre_usuario($nombre_usuario);
+    $dao = new daoUsuario();
+    $usuario_datos = $dao->get_usuario_by_nombre_usuario($nombre_usuario);
     $usuario = new Usuario($usuario_datos[2], $usuario_datos[0], $usuario_datos[1], $usuario_datos[3]);
 
     return $usuario;
