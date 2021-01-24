7a771bf01d5572ae57ab4e5f44b7f7e41067c5e7
raqueltezme <rabenzmer@gmail.com>
2021-01-24 17:58:04 +0100

    Update daoFavoritos.php

--- daoFavoritos.php
+++ daoFavoritos.php
@@ -1,8 +1,44 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+include_once '../Conontroladores/AnunciosController.php';
+
+class daoFavoritos {
+
+    public $conObj;
+    public $conexion;
+
+    function __construct() {
+        $this->conObj = new Conexion();
+        $this->conexion = $this->conObj->establecer_conexion();
+    }
+
+    function destruct() {
+        $this->conObj = new Conexion();
+        $this->conexion = null;
+        $this->conObj->cerrar_conexion();
+    }
+
+    function listar_favoritos() {
+        $sentence = "SELECT `favorito`.`id_anuncio` FROM `favorito`";
+        $id_anuncios_favoritos = mysqli_query($this->conexion, $sentence);
+
+        $anuncios = listAllAnuncios();
+
+        while (mysqli_fetch_row($id_anuncios_favoritos)) {
+            for ($i = 0; $i < sizeof($anuncios); $i++) {
+                if ($anuncios[i][0] == $id_anuncios_favoritos) {
+                    array_push($favoritos, $anuncios[i]);
+                }
+            }
+        }
+
+        return $favoritos;
+    }
+
+}
