dbf05d34d6d7e401c4cde91095118de9de727f1e
manum <manuxd29@gmail.com>
2021-01-24 11:46:56 +0100

    Busqueda

--- Controladores/indexController.php
+++ Controladores/indexController.php
@@ -3,7 +3,7 @@
 include_once '../Controladores/AnuncioController.php';
 
 if (isset($_POST['realizar_busqueda'])) {
-    $palabras_clave = split(' ', $_POST['barra_buscador']);
+    $palabras_clave = preg_split(' ', $_POST['barra_buscador']);
 
     foreach ($_POST['tipo_oferta'] as $filtro) {
         $palabras_clave .= $filtro;
