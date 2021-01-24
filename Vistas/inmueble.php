7b4750e3ce893d2102c96c331290bf0d3cdc2a8f
raqueltezme <rabenzmer@gmail.com>
2021-01-24 13:27:56 +0100

    .

--- Vistas/inmueble.php
+++ Vistas/inmueble.php
@@ -1,8 +1,59 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!doctype html>
+<html>
+    <head>
+        <title>Detalle inmueble</title>
+        <link rel="stylesheet" href="../Bootstrap/css/landing-page.css"/>
+        <link rel="stylesheet" href="../Bootstrap/vendor/bootstrap/css/bootstrap.css"/>
+        <script src="../scripts.js"></script>
+        <?php
+        include_once '../Vistas/header.php';
+        include_once '../Controladores/InmueblesController.php';
+        ?>
+    </head>
+    <body>
+        <header class="masthead">
+            <?php
+            if (isset($_SESSION['usuario_particular']) || isset($_SESSION['usuario_profesional'])) {
+                sesion_iniciada();
+            } elseif (isset($_SESSION['admin'])) {
+                cabecera_admin();
+            } else {
+                no_sesion_iniciada();
+            }
+            ?>
+        </header>
+        <?php
+        if (!isset($_POST['realizar_busqueda'])) {
+            $dirección;
+            $datos;
+            ?>
+            <main>
+                <article>
+                    <section id="detalle_inmueble">
+                    <h1><?php echo $dirección; ?></h1>
+                    <ul>
+                        <?php 
+                        //Galería de imágenes del inmueble
+                        ?>
+                    </ul>
+                    <table>
+                        <?php 
+                        //Datos del inmueble
+                        ?>
+                    </table>
+                    </section>
+                    <section id="resenyas_inmueble">
+                        <button id="nueva_resenya" onclick="nueva_resenya()">Escribir una reseña</button>
+                        <?php
+                        //Hacer una tabla por cada resenya asociada a este anuncio
+                        ?>
+                    </section>
+                </article>
+            </main> <?php
+        }
+        ?>
+    </body>
+    <?php
+    include_once '../Vistas/footer.html';
+    ?>
+</html>
