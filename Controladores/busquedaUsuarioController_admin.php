8db878b631d850714fd593490a600e4b69efa1dc
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2021-01-25 19:42:02 +0100

    Delete busquedaUsuarioController_admin.php

--- Controladores/busquedaUsuarioController_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_POST['busuario'])) {
-    mostrarVistaLista();
-}
-
-function mostrarVistaLista() {
-    echo '<tr>' + '<a href="<!-- ENLACE AL USUARIO -->"> ' + '<!-- NOMBRE USUARIO --> </tr>';
-}
