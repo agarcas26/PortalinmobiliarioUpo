36db46832b8b35e066847e8222438157a7d02fa0
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2021-01-25 05:00:21 +0100

    la puta s

--- Controladores/UsuarioController.php
+++ Controladores/UsuarioController.php
@@ -4,7 +4,7 @@ include_once '../DAO/daoUsuarios.php';
 include_once '../Modelos/UsuarioModel.php';
 
 function getUsuarioByUsuario($nombre_usuario) {
-    $dao = new daoUsuario();
+    $dao = new daoUsuarios();
     $usuario_datos = $dao->get_usuario_by_nombre_usuario($nombre_usuario);
     $usuario = new Usuario($usuario_datos[2], $usuario_datos[0], $usuario_datos[1], $usuario_datos[3]);
 
