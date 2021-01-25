3425a5dfbbf87af2bdd20f0842f7682156f10435
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2021-01-25 20:35:46 +0100

    Update busquedaController.php

--- Controladores/busquedaController.php
+++ Controladores/busquedaController.php
@@ -34,15 +34,12 @@ function mostrarVistaLista() {
     $dao->destruct();
     for ($i = 0; $i < sizeof($array_anuncios); $i++) {
         echo '<tr>';
-        echo '<a href="../Vistas/detalle_anuncio.php">';
+        echo '<a href="../Vistas/detalle_anuncio.php?id_anuncio='.$array_anuncios[$i][0].'">';
         echo '<td>' . '</td>';    //Insertar imágenes
         echo '<td>' . $array_anuncios[i][1] . '</td>';
         echo '<td>' . $array_anuncios[i][7] . '</td>';
         echo '<td>' . $array_anuncios[i][8] . '</td>';
-        echo '<form>';
-        echo '<td>' . '<input type="submit" name="ver_detalle" id="ver_detalle" value="Ver detalle" />';
-        echo '<input type="hidden" name="id_anuncio" id="id_anuncio" value="' . $anuncios[$i][0] . '" />' . '</td>';
-        echo '</form>';
+        echo '<td>' . '<button name="ver_detalle" id="ver_detalle" value="Ver detalle" /></td>';
         echo '</a>';
         echo '</tr>';
     }
@@ -54,12 +51,11 @@ function mostrarVistaCuadricula() {
     $dao->destruct();
     for ($i = 0; $i < sizeof($array_anuncios); $i++) {
         echo '<tr>';
-        echo '<a href="../Vistas/detalle_anuncio.php">';
+        echo '<a href="../Vistas/detalle_anuncio.php?id_anuncio='.$array_anuncios[$i][0].'">';
         echo '<td>' . '</td>';    //Insertar imágenes
         echo '<td>' . $array_anuncios[i][7] . '</td>';
         echo '<form>';
-        echo '<td>' . '<input type="submit" name="ver_detalle" id="ver_detalle" value="Ver detalle" />';
-        echo '<input type="hidden" name="id_anuncio" id="id_anuncio" value="' . $anuncios[$i][0] . '" />' . '</td>';
+        echo '<td>' . '<input type="submit" name="ver_detalle" id="ver_detalle" value="Ver detalle" />'.'</td>';
         echo '</form>';
         echo '</a>';
         echo '</tr>';
