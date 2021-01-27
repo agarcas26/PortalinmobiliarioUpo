0d76a6464c271321e019bad332a117d0a667daa4
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2021-01-27 11:45:37 +0100

    Update AlertasController.php

--- Controladores/AlertasController.php
+++ Controladores/AlertasController.php
@@ -40,7 +40,7 @@ function get_alertas_usuario() {
         echo '</tr><tr>';
         echo '<td>Precio máximo: ' . $alertas[$i][6] . '€</td>';
         echo '</tr><tr>';
-        echo '<td><button onclick="eliminar_alerta('.$alertas[$i][0].');" type="button">Eliminar alerta</button></td>';
+        echo '<td><button onclick="eliminar_alerta('.$alertas[$i][0].')" type="button">Eliminar alerta</button></td>';
         echo '</tr>';
         echo '</table>';
     }
