72f7e17cf75c9bef1bcc1b08667613cbb880e26d
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2021-01-27 14:07:15 +0100

    Update pago.php

--- Vistas/pago.php
+++ Vistas/pago.php
@@ -1,8 +1,10 @@
 <!doctype html>
 <html>
     <head>
+        <link rel="stylesheet" href="../Bootstrap/css/landing-page.css"/>
+        <link rel="stylesheet" href="../Bootstrap/vendor/bootstrap/css/bootstrap.css"/>
         <title>Pago</title>
-        <?php        
+        <?php
         include_once '../Vistas/header.php';
         ?>
         <script type="text/javascript">
@@ -23,36 +25,42 @@
     <body>
         <header class="masthead text-white text-center">
             <?php
-            if (isset($_SESSION['usuario'])) {
+            if (isset($_SESSION['usuario_particular']) || isset($_SESSION['usuario_profesional'])) {
                 sesion_iniciada();
             } elseif (isset($_SESSION['admin'])) {
                 cabecera_admin();
+                $usuario = $_SESSION['admin'];
             } else {
                 no_sesion_iniciada();
             }
-            ?>
-        </header>
-        <main>
-            <form action="pagoController.php" method="POST">
-                <input type="submit" name="paypal" value="PayPal" />
-                <input type="submit" name="visa" onclick="datosTarjeta()" value="Visa" />
-                <table id="datos_visa">
-                </table>
-                <table>
-                    <tr>
-                        <td><strong>Datos del inmueble</strong></td>
-                        <td><strong>Datos del usuario</strong></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </table>
-                <input type="submit" name="guardar" value="Confirmar pago" />
-            </form>
-        </main>
-    </body>
-    <?php
-    include_once '../Vistas/footer.html';
-    ?>
+            if (isset($_SESSION['usuario_particular'])) {
+                $usuario = $_SESSION['usuario_particular'];
+            } else if (isset($_SESSION['usuario_profesional'])) {
+                $usuario = $_SESSION['usuario_profesional'];
+            }
+                ?>
+            </header>
+            <main>
+                <form action="pagoController.php" method="POST">
+                    <input class="btn btn-primary" type="submit" name="paypal" value="PayPal" />
+                    <input class="btn btn-primary" type="submit" name="visa" onclick="datosTarjeta()" value="Visa" />
+                    <table id="datos_visa">
+                    </table>
+                    <table>
+                        <tr>
+                            <td><strong>Datos del inmueble</strong></td>
+                            <td><strong>Datos del usuario</strong></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><?php echo $usuario; ?></td>
+                        </tr>
+                    </table>
+                    <input class="btn-block btn-secondary" type="submit" name="guardar" value="Confirmar pago" />
+                </form>
+            </main>
+        </body>
+        <?php
+        include_once '../Vistas/footer.html';
+        ?>
 </html>
