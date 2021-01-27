a543358a1d6ff95620a732a096ff528ccf81ada9
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2021-01-27 23:01:38 +0100

    .

--- Controladores/checkout_controller.php
+++ Controladores/checkout_controller.php
@@ -1,5 +1,10 @@
 <?php
-
+require_once '../Dao/daoAnuncios.php';
+require_once '../Dao/daoAlquiler.php';
+require_once '../Dao/daoCompra.php';
+require_once '../Modelos/AnunciosModel.php';
+require_once '../Modelos/AlquilerModel.php';
+require_once '../Modelos/CompraModel.php';
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +13,27 @@
 if($_POST["payment_status"]!="VERIFIED"){
     header("Location: ../Vistas/busqueda.php");
 }else{
-    $_GET["id_anuncio"];
+    if (isset($_SESSION['usuario_particular'])) {
+        $usuario = $_SESSION['usuario_particular'];
+    } else {
+        $usuario = $_SESSION['usuario_profesional'];
+    }
+    $dao=new daoAnuncios();
+    $anuncio=$dao->get_tipo_anuncio($_GET["id_anuncio"]);
+    $dao->destruct();
+    if($anuncio=="Compra"){
+        $dao=new daoCompra();
+        $dao->crear_compra($usuario, $anuncio, $señal);
+        
+        $dao->destruct();
+    }else{
+        $dao=new daoAlquiler();
+        $dao->crear_alquileres($usuario, $anuncio);
+        
+        $dao->destruct();
+    }
+     $dao=new daoAnuncios();
+    $dao->eliminar($_GET["id_anuncio"]);
+    $dao->destruct();
 }
 
