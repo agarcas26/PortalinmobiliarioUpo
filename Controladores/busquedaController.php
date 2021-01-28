8a63745399450013ab3d9784d5bb06bfc2b7f192
raqueltezme <rabenzmer@gmail.com>
2021-01-28 11:57:18 +0100

    .

--- Controladores/busquedaController.php
+++ Controladores/busquedaController.php
@@ -20,55 +20,63 @@ if (isset($_POST['aplicar_filtros'])) {
     $num_hab = $_POST['num_hab'];
     $m2 = $_POST['m2'];
     $fecha = $_POST['fecha'];
+
     $dao = new daoBusqueda();
     $dao->crear_busqueda($nombre_usuario, $num_banyos, $tipo_inmueble, $tipo_oferta, $precio_max, $num_hab, $m2);
     $dao->destruct();
-    mostrarVistaLista($fecha);
+
+    mostrarVistaLista();
 }
 
 if (isset($_POST['lista'])) {
     mostrarVistaLista();
 }
+
 if (isset($_POST['cuadricula'])) {
     mostrarVistaCuadricula();
 }
 
-function mostrarVistaLista($fecha) {
-    $dao = new daoAnuncios();
-    if (mysqli_num_rows($dao->listar()) > 0) {
-        $array_anuncios = $dao->listar();
-        $dao->destruct();
-        while ($fila = mysqli_fetch_array($array_anuncios)) {
-            if ($fila[8] > $fecha) {
-                echo '<tr>';
-                echo '<td>' . '</td>';    //Insertar imágenes
-                echo '<td>' . $fila[1] . '</td>';
-                echo '<td>' . $fila[7] . '</td>';
-                echo '<td>' . $fila[8] . '</td>';
-                echo '<td>' . '<a href="../Vistas/detalle_anuncio.php?id_anuncio=' . $fila[0] . '">'
-                . '  <button name="ver_detalle" id="ver_detalle" value="Ver detalle">Ver detalle</button></a></td>'
-                . '<td>' . '<a href="../Vistas/pago.php?id_anuncio=' . $fila[0] . '">'
-                . '  <button name="transaccion" id="transaccion" value="transaccion">¡Lo quiero!</button></a></td>';
-                echo '</tr>';
-            }
-        }
+if (isset($_GET["id_busqueda"])) {
+    if (isset($_SESSION['usuario_particular'])) {
+        $nombre_usuario = $_SESSION['usuario_particular'];
+    } elseif (isset($_SESSION['usuario_profesional'])) {
+        $nombre_usuario = $_SESSION['usuario_profesional'];
+    }
+
+    $dao = new daoBusqueda();
+    $busquedas_usuario = $dao->eliminar_alerta_usuario($_GET["id_busqueda"]);
+    $dao->destruct();
+    unset($_GET["id_busqueda"]);
+    header("Location: ../Vistas/mis_alertas.php");
+}
+
+function mostrarVistaLista() {
+    $anuncios = anuncios_busqueda();
+    for ($i = 0; $i < sizeof($anuncios); $i++) {
+        echo '<tr>';
+        echo '<td>' . '</td>';    //Insertar imágenes
+        echo '<td>' . $anuncios[$i]->getTipo_via() . '</td>';
+        echo '<td>' . $anuncios[$i]->getNombre_via() . '</td>';
+        echo '<td>' . $anuncios[$i]->getPrecio() . '</td>';
+        echo '<td>' . $anuncios[$i]->getFecha_anuncio() . '</td>';
+        echo '<td>' . '<a href="../Vistas/detalle_anuncio.php?id_anuncio=' . $anuncios[$i]->getId_anuncio() . '">'
+        . '  <button name="ver_detalle" id="ver_detalle" value="Ver detalle">Ver detalle</button></a></td>'
+        . '<td>' . '<a href="../Vistas/pago.php?id_anuncio=' . $anuncios[$i]->getId_anuncio() . '">'
+        . '  <button name="transaccion" id="transaccion" value="transaccion">¡Lo quiero!</button></a></td>';
+        echo '</tr>';
     }
 }
 
 function mostrarVistaCuadricula() {
-    $dao = new daoAnuncios();
-    $array_anuncios = $dao->listar();
-    $dao->destruct();
-    for ($i = 0; $i < sizeof($array_anuncios); $i++) {
-        if ($fila[8] > $fecha) {
-            echo '<tr>';
-            echo '<td>' . '</td>';    //Insertar imágenes
-            echo '<td>' . $array_anuncios[$i][7] . '</td>';
-            echo '<form>';
-            echo '<td>' . '<a href="../Vistas/detalle_anuncio.php?id_anuncio=' . $array_anuncios[$i][0] . '"><button name="ver_detalle" id="ver_detalle" value="Ver detalle">Ver detalle</button></a>' . '</td>';
-            echo '</form>';
-            echo '</tr>';
-        }
+    $anuncios = anuncios_busqueda();
+    for ($i = 0; $i < sizeof($anuncios); $i++) {
+        echo '<tr>';
+        echo '<td>' . '</td>';    //Insertar imágenes
+        echo '<td>' . $anuncios[$i]->getPrecio() . '</td>';
+        echo '<form>';
+        echo '<td>' . '<a href="../Vistas/detalle_anuncio.php?id_anuncio=' . $anuncios[$i]->getId_anuncio() . '"><button name="ver_detalle" id="ver_detalle" value="Ver detalle">Ver detalle</button></a>' . '</td>';
+        echo '</form>';
+        echo '</tr>';
     }
 }
 
@@ -124,45 +132,6 @@ function get_ultimas_busquedas_usuario() {
     }
 }
 
-function print_resultados_busqueda() {
-    $barra_busqueda = $_POST["barra_busqueda"];
-    $anuncios[] = anuncios_barra_busqueda($barra_busqueda);
-    echo"<tr>"
-    . "<th>Titulo<th>"
-    . "<th>Fecha Publicacion<th>"
-    . "<th>Publicado por<th>"
-    . "<th>Direccion<th>";
-    for ($i = 0; $i < siceof($anuncios); $i++) {
-        echo "<td>" . $anuncios[$i]->getPrecio() . "</td>"
-        . "<td>" . $anuncios[$i]->getTitulo() . "</td>"
-        . "<td>" . $anuncios[$i]->getFecha_anuncio() . "</td>"
-        . "<td>" . $anuncios[$i]->getNombre_usuario_publica() . "</td>"
-        . "<td>" . $anuncios[$i]->getNombre_via() . " "
-        . $anuncios[$i]->getNumero() . " "
-        . $anuncios[$i]->getCp() . "</td>";
-    }
-    echo "</tr>";
-}
-
-function print_resultados_filtros() {
-    $anuncios[] = anuncios_busqueda();
-    echo"<tr>"
-    . "<th>Titulo<th>"
-    . "<th>Fecha Publicacion<th>"
-    . "<th>Publicado por<th>"
-    . "<th>Direccion<th>";
-    for ($i = 0; $i < siceof($anuncios); $i++) {
-        echo "<td>" . $anuncios[$i]->getPrecio() . "</td>"
-        . "<td>" . $anuncios[$i]->getTitulo() . "</td>"
-        . "<td>" . $anuncios[$i]->getFecha_anuncio() . "</td>"
-        . "<td>" . $anuncios[$i]->getNombre_usuario_publica() . "</td>"
-        . "<td>" . $anuncios[$i]->getNombre_via() . " "
-        . $anuncios[$i]->getNumero() . " "
-        . $anuncios[$i]->getCp() . "</td>";
-    }
-    echo "</tr>";
-}
-
 function listar_busquedas_usuario() {
 
     if (isset($_SESSION['usuario_particular'])) {
@@ -191,16 +160,42 @@ function listar_alertas_usuario() {
     return $busquedas_usuario;
 }
 
-if (isset($_GET["id_busqueda"])) {
-    if (isset($_SESSION['usuario_particular'])) {
-        $nombre_usuario = $_SESSION['usuario_particular'];
-    } elseif (isset($_SESSION['usuario_profesional'])) {
-        $nombre_usuario = $_SESSION['usuario_profesional'];
-    }
+//function print_resultados_busqueda() {
+//    $barra_busqueda = $_POST["barra_busqueda"];
+//    $anuncios[] = anuncios_barra_busqueda($barra_busqueda);
+//    echo"<tr>"
+//    . "<th>Titulo<th>"
+//    . "<th>Fecha Publicacion<th>"
+//    . "<th>Publicado por<th>"
+//    . "<th>Direccion<th>";
+//    for ($i = 0; $i < siceof($anuncios); $i++) {
+//        echo "<td>" . $anuncios[$i]->getPrecio() . "</td>"
+//        . "<td>" . $anuncios[$i]->getTitulo() . "</td>"
+//        . "<td>" . $anuncios[$i]->getFecha_anuncio() . "</td>"
+//        . "<td>" . $anuncios[$i]->getNombre_usuario_publica() . "</td>"
+//        . "<td>" . $anuncios[$i]->getNombre_via() . " "
+//        . $anuncios[$i]->getNumero() . " "
+//        . $anuncios[$i]->getCp() . "</td>";
+//    }
+//    echo "</tr>";
+//}
 
-    $dao = new daoBusqueda();
-    $busquedas_usuario = $dao->eliminar_alerta_usuario($_GET["id_busqueda"]);
-    $dao->destruct();
-    unset($_GET["id_busqueda"]);
-    header("Location: ../Vistas/mis_alertas.php");
-}
+//function print_resultados_filtros() {
+//    $anuncios[] = anuncios_busqueda();
+//    echo"<tr>"
+//    . "<th>Titulo<th>"
+//    . "<th>Fecha Publicacion<th>"
+//    . "<th>Publicado por<th>"
+//    . "<th>Direccion<th>";
+//    for ($i = 0; $i < siceof($anuncios); $i++) {
+//        echo "<td>" . $anuncios[$i]->getPrecio() . "</td>"
+//        . "<td>" . $anuncios[$i]->getTitulo() . "</td>"
+//        . "<td>" . $anuncios[$i]->getFecha_anuncio() . "</td>"
+//        . "<td>" . $anuncios[$i]->getNombre_usuario_publica() . "</td>"
+//        . "<td>" . $anuncios[$i]->getNombre_via() . " "
+//        . $anuncios[$i]->getNumero() . " "
+//        . $anuncios[$i]->getCp() . "</td>";
+//    }
+//    echo "</tr>";
+//}
+
--- Controladores/indexController.php
+++ Controladores/indexController.php
@@ -3,16 +3,10 @@
 include_once '../Controladores/AnunciosController.php';
 
 if (isset($_POST['realizar_busqueda'])) {
-    $palabras_clave = preg_split(' ', $_POST['barra_buscador']);
-
-    foreach ($_POST['tipo_oferta'] as $filtro) {
-        $palabras_clave .= $filtro;
-    }
-
 
     if (preg_match("/ab+c/", $_POST['barra_buscador'])) {
         filter_var($_POST['barra_buscador'], FILTER_SANITIZE_STRING);
-        anuncios_barra_busqueda($_POST['barra_buscador']);
+        anuncios_barra_busqueda(preg_split(" ", $_POST['barra_buscador']));
     }
 
     header("Location: ../Vistas/busqueda.php");
