c4611aa546f513e91a2982e9e74eccd4787bb54e
mtoromateos <53802745+mtoromateos@users.noreply.github.com>
2021-01-29 20:25:01 +0100

    .

--- scripts.js
+++ scripts.js
@@ -31,18 +31,12 @@ function muestraEstrellas() {
     }
 }
 
-function datosTarjeta() {
-    var tabla_datos = document.getElementById("datos_visa");
-    tabla_datos.innerHTML("<tr>"
-            + "<td><label>Nombre titular</label><input type='text' name='titular'/></td>"
-            + "</tr><tr>"
-            + "<td><label>Número tarjeta</label><input type='number' name='num_tarjeta'/></td>"
-            + "</tr>"
-            + "<tr>"
-            + "<td><label>Caducidad</label><input type='text' name='caducidad' /></td>"
-            + "<td><label>CVV</label><input type='password' name='cvv' /></td>"
-            + "</tr>");
-}
+$(document).ready(function () {
+    //$(".form-visa").hide();
+    $("#visa").click(function () {
+        $(".form-visa").toggle("fast");
+    });
+});
 
 $(document).ready(function () {
     $('table.display').DataTable();
