3e138f88b4c4c20638b5ad85b9fee72353013fa6
Andrea Garcia <agarcas@alu.upo.es>
2021-01-30 21:51:36 +0100

    dsdd

--- Vistas/modificar_inmueble.php
+++ Vistas/modificar_inmueble.php
@@ -119,18 +119,20 @@
                                 <tr>
                                     <td>
                                         <label>Cocina amueblada</label>
-                                        <input type="radio" name="txtCocina" id="si" value="<?php echo $datos[9]; ?>"/>
+                                        <input type="radio" name="txtCocina" id="si" value="si" <?php if($datos[9]=="si") echo "checked"; ?>/>
                                         <label for="si">Si</label>
-                                        <input type="radio" name="txtCocina" id="no" value="<?php echo $datos[9]; ?>"/>
+                                        <input type="radio" name="txtCocina" id="no" value="no"<?php if($datos[9]=="no") echo "checked"; ?>
+                                        
+                                        />
                                         <label for="no">No</label> 
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         <label>Tipo de inmueble</label>
-                                        <input type="radio" name="txtTipo_Inmueble" id="alquiler" value="<?php echo $datos[10]; ?>"/>
+                                        <input type="radio" name="txtTipo_Inmueble" id="alquiler" value="alquiler"<?php if($datos[10]=="alquiler")echo "checked"; ?>/>
                                         <label for="alquiler">Alquiler</label>
-                                        <input type="radio" name="txtTipo_Inmueble" id="compra" value="<?php echo $datos[10]; ?>"/>
+                                        <input type="radio" name="txtTipo_Inmueble" id="compra" value="compra"<?php if($datos[10]=="compra")echo "checked"; ?>/>
                                         <label for="compra">Compra</label>
                                     </td>
                                 </tr>
