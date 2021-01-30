66adb1ad4af690f9cd3ee3ba50d008f08fbb37c2
raqueltezme <rabenzmer@gmail.com>
2021-01-30 12:19:09 +0100

    Update detalles_inmueble.php

--- Vistas/detalles_inmueble.php
+++ Vistas/detalles_inmueble.php
@@ -7,6 +7,8 @@
         <link rel = "stylesheet" href = "../Bootstrap/vendor/bootstrap/css/bootstrap.css"/>
         <link rel="stylesheet" href="../mycss.css"/>
         <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
         <script src="../scripts.js"></script>
         <?php
         include_once '../Vistas/header.php';
