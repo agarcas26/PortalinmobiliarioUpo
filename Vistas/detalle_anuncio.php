bc70485f5bbb703d515412e9472c6daee2b61768
raqueltezme <rabenzmer@gmail.com>
2021-01-30 12:09:27 +0100

    .

--- Vistas/detalle_anuncio.php
+++ Vistas/detalle_anuncio.php
@@ -40,17 +40,13 @@
                             <?php
                             mostrar_detalle_anuncio($_GET["id_anuncio"]);
                             ?>
-                            <button>
-                                <span class="favorito" val="inactiva">
-                                    <img class="corazon" id="inactiva" src="../img/nofav.png">
-                                </span>
-                            </button>
-                            <button>
-                                <span class="alerta" val="inactiva">
-                                    <img class="campana" id="inactiva" src="../img/noalerta.png">
-                                </span>
-                            </button>
                         </table>
+                        <span class="favorito" val="inactiva">
+                            <img class="corazon" id="inactiva" src="../img/nofav.png">
+                        </span>
+                        <span class="alerta" val="inactiva">
+                            <img class="campana" id="inactiva" src="../img/noalerta.png">
+                        </span>
                     </section>
                     <section>
                         <?php
--- Vistas/detalles_inmueble.php
+++ Vistas/detalles_inmueble.php
@@ -32,21 +32,17 @@
                 <article>
                     <section id="ver_detalle">
                         <table id="datos_visa" class="display table-bordered" style="width:100%">
-
-
                             <form  action='../Controladores/InmueblesController.php' method='POST' style="margin-left:10px;">
                                 <table id="datos_visa" class="display table-bordered" style="width:50%">
-                                    <table id="datos_visa" class="display table-bordered" style="width:50%">
-                                        <tr>
-                                            <td>
-                                                <label>¿Desea eliminar este inmueble?</label>
-                                            </td>
-                                        </tr>
-                                        <?php
-                                        getInmuebleByDireccion($_GET['direccion']);
-                                        ?>
-                                       
-                                    </table>
+                                    <tr>
+                                        <td>
+                                            <label>¿Desea eliminar este inmueble?</label>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                    getInmuebleByDireccion($_GET['direccion']);
+                                    ?>                                       
+                                </table>
                             </form>
                         </table>
                     </section>
