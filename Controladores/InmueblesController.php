a6f180ebaa05c673fe581bd8a52b0e7c7c629b2f
Andrea Garcia <agarcas@alu.upo.es>
2021-01-30 20:34:18 +0100

    fff

--- Controladores/InmueblesController.php
+++ Controladores/InmueblesController.php
@@ -394,11 +394,13 @@ function listar_inmuebles_usuarioAll() {
         echo '<td>' . ' Código Postal :' . $inmuebles_usuario[$i]->getCp() . '</td>';
         echo '<td>' . ' - Nombre vía : ' . $inmuebles_usuario[$i]->getNombre_via() . " " . '</td>';
         echo '<td>' . ' - Tipo vía : ' . $inmuebles_usuario[$i]->getTipo_via() . " " . '</td>';
+         echo "<br> ";
         echo '<td>';
         echo '<a href="../Vistas/detalles_inmueble.php?direccion=' . $direccion . '">'
         . '<input type="submit" name="ver_detalle" id="ver_detalle" value="Ver detalle" />'
         . '</a>'
         . '</td>';
+         echo "<br>";
         echo '<td>';
         echo '<a href="../Vistas/modificar_inmueble.php?direccion=' . $direccion . '">'
         . '<input type="submit" name="btonModificar" id="btonModificar" value="Modificar " />'
