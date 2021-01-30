c21ecab2fc374e66e7a0cb13ec467c6be5d95120
raqueltezme <rabenzmer@gmail.com>
2021-01-31 00:36:53 +0100

    .

--- Vistas/detalle_anuncio.php
+++ Vistas/detalle_anuncio.php
@@ -48,20 +48,22 @@
                             <span class="favorito" val="<?php
                             if (esFavorito($_GET["id_anuncio"])) {
                                 echo"activa";
-                            } else
+                            } else {
                                 echo"inactiva";
+                            }
                             ?>">
                                 <input name="corazon" type="hidden" value="true" />
                                 <input name="corazon" type="image" class="corazon" src="../img/nofav.png">
                             </span>
                         </form>  
                         <form action="../Vistas/detalle_anuncio.php?id_anuncio=<?php echo $_GET["id_anuncio"]; ?>" id="formValoracionInmueble" class="md-form mr-auto mb-4" method="POST"> 
-                             <input name="id_anuncio" value="<?php echo $_GET["id_anuncio"]; ?>" hidden>
+                            <input name="id_anuncio" value="<?php echo $_GET["id_anuncio"]; ?>" hidden>
                             <span class="alerta" val="<?php
                             if (hayAlerta($_GET["id_anuncio"])) {
                                 echo"activa";
-                            } else
+                            } else {
                                 echo"inactiva";
+                            }
                             ?>">
                                 <input name="campana" type="hidden" value="true" />
                                 <input name="campana" type="image"  class="campana" src="../img/noalerta.png">
